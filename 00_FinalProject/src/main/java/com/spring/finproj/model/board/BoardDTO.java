5e466382d25c9ff18d9b751727d9087ac8199f6b
eneinp <eneinp0462@naver.com>
2023-05-16 12:41:53 +0900

    reff

--- 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDTO.java
+++ 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDTO.java
@@ -3,4 +3,11 @@ package com.spring.finproj.model.board;
 public class BoardDTO {
 
 	private int cm_no;
+	private int user_no;
+	private String content;
+	private String date;
+	private String update;
+	private String hashtag;
+	private String photo_folder;
+	private int photo_length;
 }
