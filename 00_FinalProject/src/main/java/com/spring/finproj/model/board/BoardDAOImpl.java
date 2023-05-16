046970b7670f06c7800230e8b1cff80834396510
eneinp <eneinp0462@naver.com>
2023-05-16 12:27:02 +0900

    ccc

--- 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDAOImpl.java
+++ 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDAOImpl.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BoardDAOImpl implements BoardDAO{
 	private SqlSessionTemplate sqlSession;
+	
+	
 }
--- 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDTO.java
+++ 00_FinalProject/src/main/java/com/spring/finproj/model/board/BoardDTO.java
@@ -2,4 +2,5 @@ package com.spring.finproj.model.board;
 
 public class BoardDTO {
 
+	
 }
