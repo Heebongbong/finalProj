53993e5a9e1cdbcb0ed053f6f0b139f8afd2e9b4
kitaehwan <128345583+kitaehwan@users.noreply.github.com>
2023-06-04 01:39:53 +0900

    ki

--- 00_FinalProject/src/main/webapp/resources/js/user/mypage.js
+++ 00_FinalProject/src/main/webapp/resources/js/user/mypage.js
@@ -450,7 +450,7 @@
 
 				},
 				input_phone: {
-					phoneLength : "유효한 휴대전화 번호를 입력하세욘",
+					phoneLength : "유효한 휴대전화 번호를 입력하세요",
 					phoneCheck : "공백, 문자 없이 입력하세요",
 					phoneUnique : "이미 등록된 번호입니다."
 				},
